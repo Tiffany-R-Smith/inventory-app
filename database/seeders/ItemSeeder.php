317237caa44873b4c7ef50bf5170ad4edb657a4b
Tiffany Smith <tiffany.r.smi@gmail.com>
2022-05-16 22:36:25 -0400

    initial_setup
    Created two models and updated the seeder to populate the items table.

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+use DB;
 
 class ItemSeeder extends Seeder
 {
@@ -12,8 +16,34 @@ class ItemSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        DB::table('items')->truncate();
+
+        $names = [
+            'Air Force',
+            'Blazer',
+            'React Infinity Run',
+            'Air Zoom',
+            'Go'
+        ];
+
+        $prices = [
+            '100.00',
+            '100.00',
+            '160.00',
+            '120.00',
+            '130.00'
+        ];
+
+        for ($i = 0; $i < count($names); $i++) {
+            Item::create([
+                'name'       => $names[$i],
+                'brand'      => 'Nike',
+                'price'      => $prices[$i],
+                'about'      => $faker->text,
+                'created_at' => Carbon::now()
+            ]);
+        }
     }
 }
