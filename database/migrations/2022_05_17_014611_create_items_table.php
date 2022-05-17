f809b503241a06910ff8e7fcb1495432b7de2eab
Tiffany Smith <tiffany.r.smi@gmail.com>
2022-05-16 22:09:55 -0400

    initial_setup
    Created two migration files for the items and inventories tables

---
+++ database/migrations/2022_05_17_014611_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('brand')->nullable();
+            $table->decimal('price', 5, 2);
+            $table->text('about');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
